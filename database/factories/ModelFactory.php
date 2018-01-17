25acb681eaa357290e99c6a2a45e2588cb4d506b
julio <julio_filgueiras@live.com>
2018-01-16 22:25:01 -0200

    crud animais - trabalhando com factory e seed

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,13 @@ $factory->define(\App\Models\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(\App\Models\Animal::class, function (Faker\Generator $faker) {
+
+
+    return [
+        'name' => $faker->name,
+        'race' => $faker->colorName,
+        'weight' => $faker->numberBetween(1, 50)
+    ];
+});
