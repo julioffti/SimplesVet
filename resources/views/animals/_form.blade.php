1292e5325a38b67e2fcdbd803b25ffd9eba8ae28
julio <julio_filgueiras@live.com>
2018-01-19 22:15:48 -0200

    recuparando dados do formulario

--- resources/views/animals/_form.blade.php
+++ resources/views/animals/_form.blade.php
@@ -1,14 +1,14 @@
 {{csrf_field()}}
 <div class="form-group">
     <label for="name">Nome</label>
-    <input class="form-control" id="name" name="name" value="{{$animal->name}}">
+    <input class="form-control" id="name" name="name" value="{{old('name', $animal->name)}}">
 </div>
 <div class="form-group">
     <label for="race">Raça</label>
-    <input class="form-control" id="race" name="race" value="{{$animal->race}}">
+    <input class="form-control" id="race" name="race" value="{{old ('race', $animal->race)}}">
 </div>
 <div class="form-group">
     <label for="weight">Peso</label>
-    <input class="form-control" id="weight" name="weight" value="{{$animal->weight}}">
+    <input class="form-control" id="weight" name="weight" value="{{old('weight', $animal->weight)}}">
 </div>
 <br/>
