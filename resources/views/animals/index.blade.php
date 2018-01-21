c5b28349e6f4a686c5df2d0eaac022342e935b7f
julio <julio_filgueiras@live.com>
2018-01-21 04:34:41 -0200

    ajustando pesquisa na view index

--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -5,58 +5,76 @@
         .buttons > a{
             font-size: 16px;
             display: inline-block;
-            padding: 12px 12px 10px 12px;
+            padding: 9px 11px 9px 11px;
             border-radius: 20px;
+        }
+        .buttons > button{
+            padding: 13px 15px 13px 15px;
+            border-radius: 20px;
+        }
+
+        .form-group > button{
+            postion: relative;
+            margin-left: 101%;
+            margin-top: -4.8%;
+            size: 10px;
+            padding: 15px 15px 15px 15px;
+            border-radius: 30px;
+        }
+        .form-group > a {
+            postion: relative;
+            margin-left: 106%;
+            margin-top: -6.8%;
+            size: 10px;
+            padding: 15px 15px 15px 15px;
+            border-radius: 30px;
         }
     </style>
 <br/><br/>
-
     <div class="row">
         <form method="GET" action="{{route('animals.store')}}" class="form">
             <div class="form-group">
-                <label for="busca">Buscar animais</label>
+                <label for="busca"><h3>Buscar clientes</h3></label>
+                <br/>
                 <input class="form-control" name="busca" id="busca" type="text" placeholder="Digite aqui a sua busca">
-
-                </br>
-
-                <div class="form-group">
-                    <input class="btn btn-primary" value="Buscar animal" type="submit">
-                </div>
             </div>
-            </br>
+            <div class="form-group">
+                <button class="btn btn-primary" value="Buscar animal" type="submit"><span class="glyphicon glyphicon-search"></span></button> &nbsp;&nbsp;
+                <a class="btn btn-info" href="{{route('animals.index')}}"><span class="glyphicon glyphicon-step-backward"></span></a>
+            </div>
         </form>
     </div>
 
-<div class="buttons">
-<a class="btn btn-primary" href="{{route('animals.create')}}">Cadastrar Animal</a>
-</div>
-<br/><br/>
-<table class="table table striped">
-    <thead>
-    <tr>
-        <th>ID</th>
-        <th>Nome</th>
-        <th>Raça</th>
-        <th>Peso</th>
-        <th>Ação</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($animals as $animal)
+    <div class="buttons">
+    <a class="btn btn-primary" href="{{route('animals.create')}}">Cadastrar Animal</a>
+    </div>
+    <br/><br/>
+    <table class="table table striped">
+        <thead>
         <tr>
-            <td>{{$animal->id}}</td>
-            <td>{{$animal->name}}</td>
-            <td>{{$animal->race}}</td>
-            <td>{{$animal->weight}}</td>
-            <td>
-                <div class="buttons">
-                <a href="{{route('animals.show', ['animal' => $animal->id])}}" class="btn btn-default btn-md"><span class="glyphicon glyphicon-eye-open">&nbsp;Ver</span></a>&nbsp;&nbsp;
-                <a href="{{route('animals.edit', ['animal' => $animal->id])}}" class="btn btn-info"><span class="glyphicon glyphicon-pencil">&nbsp;Editar</span></a>
-                </div>
-            </td>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Raça</th>
+            <th>Peso</th>
+            <th>Ação</th>
         </tr>
-    @endforeach
-    </tbody>
-</table>
-    {{$animals->links()}}
-@endsection
+        </thead>
+        <tbody>
+        @foreach($animals as $animal)
+            <tr>
+                <td>{{$animal->id}}</td>
+                <td>{{$animal->name}}</td>
+                <td>{{$animal->race}}</td>
+                <td>{{$animal->weight}}</td>
+                <td>
+                    <div class="buttons">
+                    <a href="{{route('animals.show', ['animal' => $animal->id])}}" class="btn btn-default btn-md"><span class="glyphicon glyphicon-eye-open">&nbsp;Ver</span></a>&nbsp;&nbsp;
+                    <a href="{{route('animals.edit', ['animal' => $animal->id])}}" class="btn btn-info"><span class="glyphicon glyphicon-pencil">&nbsp;Editar</span></a>
+                    </div>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+        {{$animals->links()}}
+    @endsection
--- resources/views/animals/show.blade.php
+++ resources/views/animals/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <style>
         .buttons > a{
-            font-size: 16px;
+            font-size: 18px;
             display: inline-block;
             padding: 12px 12px 10px 12px;
             border-radius: 20px;
@@ -11,9 +11,9 @@
     </style>
 </br>
 <div class="buttons">
-<a href="{{route('animals.edit', ['animal' => $animal->id])}}" class="btn btn-info">Editar</a>&nbsp;&nbsp;
+<a href="{{route('animals.edit', ['animal' => $animal->id])}}" class="btn btn-info"><span class="glyphicon glyphicon-pencil">&nbsp;Editar</span></a>&nbsp;&nbsp;
 <a class="btn btn-danger" href="{{ route('animals.destroy',['animal' => $animal->id]) }}"
-   onclick="event.preventDefault();if(confirm('Deseja excluir este animal??')){document.getElementById('form-deletar-{{$animal->id}}').submit();}">Excluir</a>
+   onclick="event.preventDefault();if(confirm('Deseja excluir este animal??')){document.getElementById('form-deletar-{{$animal->id}}').submit();}"><span class="glyphicon glyphicon-trash">&nbsp;Excluir</span></a>
 <form id="form-deletar-{{$animal->id}}"style="display: none" action="{{ route('animals.destroy',['client' => $animal->id]) }}" method="post">
     {{csrf_field()}}
     {{method_field('DELETE')}}
